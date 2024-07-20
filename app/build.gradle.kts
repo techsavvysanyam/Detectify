0e190aa71e49808e4a78ba5ef66f626afd8a5d14
Sanyam Soni <105146300+Sanyamsoni77@users.noreply.github.com>
2024-07-20 16:46:11 +0530

    new additions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "2.3"
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
